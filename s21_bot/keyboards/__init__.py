74a2e53f8b38e855553990d4873deda1f92b384c
darkned <darken.mail@mail.ru>
2026-05-06 01:17:18 +0300

    Refine cabinet navigation and command help

--- s21_bot/keyboards/__init__.py
+++ s21_bot/keyboards/__init__.py
@@ -3,7 +3,7 @@ from .inline import (
     cooldown_kb, decided_kb, failed_auth_kb, ban_duration_kb,
     reject_reason_input_kb, cooldown_with_reason_kb,
     verification_request_kb, verification_result_kb,
-    cabinet_kb,
+    cabinet_kb, cabinet_home_kb, cabinet_back_kb,
 )
 
 __all__ = [
@@ -11,5 +11,5 @@ __all__ = [
     "cooldown_kb", "decided_kb", "failed_auth_kb", "ban_duration_kb",
     "reject_reason_input_kb", "cooldown_with_reason_kb",
     "verification_request_kb", "verification_result_kb",
-    "cabinet_kb",
+    "cabinet_kb", "cabinet_home_kb", "cabinet_back_kb",
 ]
